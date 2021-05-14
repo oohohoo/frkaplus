fd058bc7ec3f18ea6a65436045b2324f0e6a5528
dejan-77 <dejankutic@me.com>
2021-05-14 07:34:21 +0200

    gr

--- app.js
+++ app.js
@@ -437,9 +437,8 @@ views: [{
 
   beforeEnter(data) {
   titleHero();
-   //heroSwiper();
-   //autoPlay();
-   console.log("autoplay!"); 
+
+  
   },
   once(data) {
     titleHero();
@@ -450,8 +449,11 @@ views: [{
 
 
 },{
-  namespace: 'projects',
+  namespace: 'onama',
   beforeEnter(){
+
+    locoAccordion();
+
     projectsHeroAnima();
     projectAnchorScroll();
     },
@@ -460,12 +462,12 @@ views: [{
     }
 
 },{
-    namespace: 'tailored',
+    namespace: 'xxxxxxxx',
   beforeEnter(data) {
     tailoredHeroAnima();
  
   }},{
-  namespace: 'contact',
+  namespace: 'xxxxxxxxxx',
   afterEnter(data) {
     contactHeroAnima();
 
@@ -881,8 +883,75 @@ fadeEffect: {
 
 /*
 ================================================================================
-VIDEO AUTOPLAY
+LOCO ACCORDION
 ================================================================================
 */
 
+function locoAccordion() {
+//jQuery Accordion Stuff
+var accordion = (function(){
 
+  var $accordion = $('.js-accordion');
+  var $accordion_header = $accordion.find('.js-accordion-header');
+  var $accordion_item = $('.js-accordion-item');
+  var delayInMilliseconds = 100; //10th of a second
+  // default settings 
+  var settings = {
+    // animation speed
+    speed: 400,
+    
+    // close all other accordion items if true
+    oneOpen: false
+  };
+    
+  return {
+    // pass configurable object literal
+    init: function($settings) {
+      $accordion_header.on('click', function() {
+           
+        accordion.toggle($(this));
+      
+        setTimeout(() => {
+          $('body, html').animate({ 
+            scrollTop: $(this).offset().top     
+           }, 500) //pause before scroll to top
+        }, 400) //duration of scroll to top
+        
+        setTimeout(function() { 
+          locoScroll.update();
+    }, delayInMilliseconds);
+      });
+      
+      $.extend(settings, $settings); 
+      
+      // ensure only one accordion is active if oneOpen is true
+      if(settings.oneOpen && $('.js-accordion-item.active').length > 1) {
+        $('.js-accordion-item.active:not(:first)').removeClass('active');
+      }
+      
+      // reveal the active accordion bodies
+      $('.js-accordion-item.active').find('> .js-accordion-body').show();
+    },
+    toggle: function($this) {
+            
+      if(settings.oneOpen && $this[0] != $this.closest('.js-accordion').find('> .js-accordion-item.active > .js-accordion-header')[0]) {
+        $this.closest('.js-accordion')
+               .find('> .js-accordion-item') 
+               .removeClass('active')
+               .find('.js-accordion-body')
+               .slideUp()
+      }
+      
+      // show/hide the clicked accordion item
+      $this.closest('.js-accordion-item').toggleClass('active');
+      $this.next().stop().slideToggle(settings.speed);
+    }
+  }
+})();
+
+$(document).ready(function(){
+  
+  accordion.init({ speed: 1000, oneOpen: true }); //duration of opening body__contents
+});
+
+}
