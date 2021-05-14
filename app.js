ce871541c749d88c4275580d2bc4ff87d076f813
dejan-77 <dejankutic@me.com>
2021-05-14 10:09:34 +0200

    d

--- app.js
+++ app.js
@@ -892,33 +892,74 @@ LOCO ACCORDION
 
 function locoAccordion() {
 
-var animations = $(".accordion-group").map(createAnimation);
 
-$(".accordion-menu").click(playAnimation);
 
-function playAnimation(event) {
-  
-  var selected = this;
-  
-  animations.each(function(i, animate) {
-    animate(selected);
-  });
-}
+//jQuery Accordion Stuff
+var accordion = (function(){
 
-function createAnimation(i, element) {
+  var $accordion = $('.js-accordion');
+  var $accordion_header = $accordion.find('.js-accordion-header');
+  var $accordion_item = $('.js-accordion-item');
+  var delayInMilliseconds = 100; //10th of a second
+  // default settings 
+  var settings = {
+    // animation speed
+    speed: 400,
     
-  var menu = element.querySelector(".accordion-menu");
-  var box  = element.querySelector(".accordion-content");
-  
-  gsap.set(box, { height: "auto"})
-  var tween = gsap.from(box, { duration:0.5, height: 0, ease: 'power1.out' }).reverse();
-  
-  return function(selected) {
+    // close all other accordion items if true
+    oneOpen: false
+  };
     
-    var reversed = selected !== menu ? true : !tween.reversed();
-    tween.reversed(reversed);
+  return {
+    // pass configurable object literal
+    init: function($settings) {
+      $accordion_header.on('click', function() {
+           
+        accordion.toggle($(this));
+      
+        setTimeout(() => {
+          $('body, html').animate({ 
+            scrollTop: $(this).offset().top     
+           }, 500) //pause before scroll to top
+        }, 400) //duration of scroll to top
+        
+        setTimeout(function() { 
+          locoScroll.update();
+          console.log("LOCOSCROLL ACCORDION UPDATE!");
+    }, delayInMilliseconds);
+      });
+      
+      $.extend(settings, $settings); 
+      
+      // ensure only one accordion is active if oneOpen is true
+      if(settings.oneOpen && $('.js-accordion-item.active').length > 1) {
+        $('.js-accordion-item.active:not(:first)').removeClass('active');
+      }
+      
+      // reveal the active accordion bodies
+      $('.js-accordion-item.active').find('> .js-accordion-body').show();
+    },
+    toggle: function($this) {
+            
+      if(settings.oneOpen && $this[0] != $this.closest('.js-accordion').find('> .js-accordion-item.active > .js-accordion-header')[0]) {
+        $this.closest('.js-accordion')
+               .find('> .js-accordion-item') 
+               .removeClass('active')
+               .find('.js-accordion-body')
+               .slideUp()
+      }
+      
+      // show/hide the clicked accordion item
+      $this.closest('.js-accordion-item').toggleClass('active');
+      $this.next().stop().slideToggle(settings.speed);
+    }
   }
-}
+})();
+
+$(document).ready(function(){
+  
+  accordion.init({ speed: 1000, oneOpen: true }); //duration of opening body__contents
+});
 
 
 
