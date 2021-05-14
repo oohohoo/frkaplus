2754da1352b5f11c8e853e77ab47f5dc497e134c
dejan-77 <dejankutic@me.com>
2021-05-14 15:36:36 +0200

    gr

--- app.js
+++ app.js
@@ -472,9 +472,11 @@ views: [{
     console.log("LOCATION MAP LOADED");
  
   }},{
-  namespace: 'xxxxxxxxxx',
+  namespace: 'lokacijesolo',
   afterEnter(data) {
-    contactHeroAnima();
+
+    swiperlocation();
+    //contactHeroAnima();
 
   }},{
   },{
@@ -1016,4 +1018,71 @@ mapo.on('mouseout', function() {
 
 
 
+}
+
+
+
+/*
+================================================================================
+SWIPER HERO
+================================================================================
+*/
+
+function swiperlocation() {
+
+
+var menu = ['', '', '', '', '', '']  
+  var mySwiper = new Swiper ('.swiper-container', {
+   loop: true,
+		speed: 1000,
+   // spaceBetween: 100,
+   // initialSlide: 0,
+    //truewrapper adoptsheight of active slide
+    autoHeight: false,
+    updateOnWindowResize: true,
+    // Optional parameters
+  //  direction: 'vertical',
+      // delay between transitions in ms
+    	autoplay: {
+				delay: 5000,
+				disableOnInteraction: false,
+},
+   // autoplayStopOnLast: false, // loop false also
+    // If we need pagination
+ 
+    
+    // Navigation arrows
+ /*   nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',*/
+    
+    // And if we need scrollbar
+    //scrollbar: '.swiper-scrollbar',
+    // "slide", "fade", "cube", "coverflow" or "flip"
+    effect: 'fade',
+    
+      pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+				type: 'bullets',
+				renderBullet: function (index, className) {
+					return '<span class="' + className + '">' + (menu[index]) + '</span>';
+},
+    // Distance between slides in px.
+  //  spaceBetween: 60,
+    //
+    slidesPerView: 1,
+    observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+  longSwipes: false,
+  
+    //
+   // centeredSlides: true,
+    //
+   // slidesOffsetBefore: 0,
+    //
+    grabCursor: true,
+  }
+  })        
+  
 }
