9f9864f399f04e7134b8b14b33e0c913baa7d3fe
dejan-77 <dejankutic@me.com>
2021-05-15 19:00:36 +0200

    gr

--- app.js
+++ app.js
@@ -237,9 +237,9 @@ videoroll.to(".bgvideo", {
     trigger: firstSection,    
     start: 'top 30%',
     end: "+=30%",    
-    /*scrub: 2,*/
+    scrub: 2,
   },
-  clipPath: 'inset(-30%)',
+  clipPath: 'inset(-60%)',
   rotate:6
 })
 
