cd1f693daa8a3ea934af94c60d86af88b3f27691
dejan-77 <dejankutic@me.com>
2021-05-15 18:10:03 +0200

    gr

--- app.js
+++ app.js
@@ -164,6 +164,60 @@ function handler2() {
 $(".fs-nav-butt").one("click", handler1);
 
 
+///////////// 
+
+/* =============================================
+MARQUEE + SCROLLTRIGGER change direction
+================================================ */
+
+let direction = 1; // 1 = forward, -1 = backward scroll
+
+const roll1 = roll(".rollingtext", {duration: 40}), // ugasi true i mijenja smjer
+      roll2 = roll(".rollingtext02", {duration: 60}),
+      roll3 = roll(".rollingtext03", {duration: 80}, true);
+ScrollTrigger.create({
+       trigger: ".cd-wrap",
+      
+       /*toggleClass: {
+        targets: '.logofrka, .navstyle, .kontakt',
+        className: 'orange'
+      }, */
+        start: "top 30%",
+        end: "bottom top",
+        scroller: ".smooth-scroll",
+            /*  invalidateOnRefresh: true, */
+         onUpdate(self) {
+          if (self.direction !== direction) {
+            direction *= -1;
+           
+            gsap.to([roll1, roll2, roll3], {
+                timeScale: direction, 
+                overwrite: true            
+            });
+
+            
+          }
+        }
+      });
+
+// helper function that clones the targets, places them next to the original, then animates the xPercent in a loop to make it appear to roll across the screen in a seamless loop.
+function roll(targets, vars, reverse) {
+  const tl = gsap.timeline({
+    repeat: -1,
+    onReverseComplete() { 
+      this.totalTime(this.rawTime() + this.duration() * 10); // otherwise when the playhead gets back to the beginning, it'd stop. So push the playhead forward 10 iterations (it could be any number)
+    }
+  });
+  vars = vars || {};
+  vars.ease || (vars.ease = "none");
+  gsap.utils.toArray(targets).forEach(el => {
+    let clone = el.cloneNode(true);
+    el.parentNode.appendChild(clone);
+    gsap.set(clone, {position: "absolute", top: el.offsetTop, left: el.offsetLeft + (reverse ? -el.offsetWidth : el.offsetWidth)});
+    tl.to([el, clone], {xPercent: reverse ? 100 : -100, ...vars}, 0);
+  });
+  return tl;
+}
 
 
 /* sort shuffle*/
@@ -1167,58 +1221,7 @@ function homeScrollTrigger() {
 
 
 
- /* =============================================
-MARQUEE + SCROLLTRIGGER change direction
-================================================ */
 
-let direction = 1; // 1 = forward, -1 = backward scroll
-
-const roll1 = roll(".rollingtext", {duration: 40}), // ugasi true i mijenja smjer
-      roll2 = roll(".rollingtext02", {duration: 60}),
-      roll3 = roll(".rollingtext03", {duration: 80}, true);
-ScrollTrigger.create({
-       trigger: ".cd-wrap",
-      
-       /*toggleClass: {
-        targets: '.logofrka, .navstyle, .kontakt',
-        className: 'orange'
-      }, */
-        start: "top 30%",
-        end: "bottom top",
-        scroller: ".smooth-scroll",
-            /*  invalidateOnRefresh: true, */
-         onUpdate(self) {
-          if (self.direction !== direction) {
-            direction *= -1;
-           
-            gsap.to([roll1, roll2, roll3], {
-                timeScale: direction, 
-                overwrite: true            
-            });
-
-            
-          }
-        }
-      });
-
-// helper function that clones the targets, places them next to the original, then animates the xPercent in a loop to make it appear to roll across the screen in a seamless loop.
-function roll(targets, vars, reverse) {
-  const tl = gsap.timeline({
-    repeat: -1,
-    onReverseComplete() { 
-      this.totalTime(this.rawTime() + this.duration() * 10); // otherwise when the playhead gets back to the beginning, it'd stop. So push the playhead forward 10 iterations (it could be any number)
-    }
-  });
-  vars = vars || {};
-  vars.ease || (vars.ease = "none");
-  gsap.utils.toArray(targets).forEach(el => {
-    let clone = el.cloneNode(true);
-    el.parentNode.appendChild(clone);
-    gsap.set(clone, {position: "absolute", top: el.offsetTop, left: el.offsetLeft + (reverse ? -el.offsetWidth : el.offsetWidth)});
-    tl.to([el, clone], {xPercent: reverse ? 100 : -100, ...vars}, 0);
-  });
-  return tl;
-}
 
 
 /*BUTTER SLIDER*/
