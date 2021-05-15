885500812d15d270968b0feb5ebcb8d2602f39c5
dejan-77 <dejankutic@me.com>
2021-05-15 04:54:54 +0200

    ng

--- app.js
+++ app.js
@@ -164,6 +164,39 @@ function handler2() {
 $(".fs-nav-butt").one("click", handler1);
 
 
+
+
+/* sort shuffle*/
+var Shuffle = window.Shuffle;
+var element = document.querySelector('.my-shuffle-container');
+var sizer = element.querySelector('.my-sizer-element');
+
+var shuffleInstance = new Shuffle(element, {
+  itemSelector: '.picture-item',
+  sizer: sizer // could also be a selector: '.my-sizer-element'
+});
+// shuffleInstance.filter('animal');
+$("#all").on("click", function(){
+   shuffleInstance.filter();
+});
+$("#btn-animal").on("click", function(){
+   shuffleInstance.filter('animal');
+});
+$("#btn-city").on("click", function(){
+   shuffleInstance.filter('city');
+});
+$("#btn-nature").on("click", function(){
+   shuffleInstance.filter('nature');
+});
+/*
+ScrollTrigger.refresh();
+console.log("SCROLLTRIGGER UPDATED AFTER SORT");
+*/
+locoScroll.update();
+console.log("LOCOSCROLL UPDATED AFTER SORT");
+
+
+
 /*
 var video = document.getElementById("bgvid");
 video.oncanplaythrough = function() {
@@ -1116,32 +1149,6 @@ SWIPER HERO
 */
 
 function sortScript() {
-  var Shuffle = window.Shuffle;
-  var element = document.querySelector('.my-shuffle-container');
-  var sizer = element.querySelector('.my-sizer-element');
-  
-  var shuffleInstance = new Shuffle(element, {
-    itemSelector: '.picture-item',
-    sizer: sizer // could also be a selector: '.my-sizer-element'
-  });
-  // shuffleInstance.filter('animal');
-  $("#all").on("click", function(){
-     shuffleInstance.filter();
-  });
-  $("#btn-animal").on("click", function(){
-     shuffleInstance.filter('animal');
-  });
-  $("#btn-city").on("click", function(){
-     shuffleInstance.filter('city');
-  });
-  $("#btn-nature").on("click", function(){
-     shuffleInstance.filter('nature');
-  });
-/*
-  ScrollTrigger.refresh();
-  console.log("SCROLLTRIGGER UPDATED AFTER SORT");
-*/
-  locoScroll.update();
-  console.log("LOCOSCROLL UPDATED AFTER SORT");
+
 
 }
