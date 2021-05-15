5b3ef2d46a40f0d2ac20bf4454de543f36a4cddf
dejan-77 <dejankutic@me.com>
2021-05-15 18:23:30 +0200

    f

--- app.js
+++ app.js
@@ -172,9 +172,11 @@ MARQUEE + SCROLLTRIGGER change direction
 
 let direction = 1; // 1 = forward, -1 = backward scroll
 
-const roll1 = roll(".rollingtext", {duration: 40}), // ugasi true i mijenja smjer
-      roll2 = roll(".rollingtext02", {duration: 60}),
-      roll3 = roll(".rollingtext03", {duration: 80}, true);
+const roll1 = roll(".roll1", {duration: 40}, true), // ugasi true i mijenja smjer
+      roll2 = roll(".roll2", {duration: 60}),
+      roll3 = roll(".roll3", {duration: 20}, true),
+      roll4 = roll(".roll4", {duration: 30}),
+      roll5 = roll(".roll5", {duration: 80}, true);
 ScrollTrigger.create({
        trigger: ".cd-wrap",
       
