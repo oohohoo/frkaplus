49275d389a2cccdba35107e30dc189c9761b0f13
dejan-77 <dejankutic@me.com>
2021-05-17 19:03:35 +0200

    d

--- app.js
+++ app.js
@@ -1525,7 +1525,7 @@ function animateOpenNav() {
   var mobileNav = document.getElementById("krug");
   tl
   .set('body', { overflow: 'hidden' })
-  .to(mobileNav, {duration: 0.6, ease: "power3.out", y: 0 })
+  .to(mobileNav, {duration: 0.6, ease: "power3.out", y: 620 })
   //.to('.kontakt-back', {y, ease: "power1.in"})
     .to(".nav__link", {opacity: 1, y: 0,duration: 0.2, stagger: {// wrap advanced options in an object
       each: 0.2,
