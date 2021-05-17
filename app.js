eb406d5e20f79f677a9ca0630444874552f03daf
dejan-77 <dejankutic@me.com>
2021-05-17 19:53:10 +0200

    fe

--- app.js
+++ app.js
@@ -1534,7 +1534,7 @@ function animateOpenNav() {
   tl
   .set('body', { overflow: 'hidden' })
   .to(mobileNav, {duration: 0.6, ease: "power1.in", scale:30 })
-  .to(".fs--menu", {duration: 0.6, ease: "power1.in", rotate:30, autoAlpha:0 })
+  .to(".fs--menu", {duration: 0.6, ease: "power1.in", autoAlpha:0 })
   
   //.to('.kontakt-back', {y, ease: "power1.in"})
     .to(".nav__link", {opacity: 1, y: 0,duration: 0.2, stagger: {// wrap advanced options in an object
