f71d1ead7a38a1d5c7b6c17c3fb4ba4e990729e7
dejan-77 <dejankutic@me.com>
2021-05-17 07:48:37 +0200

    gr

--- app.js
+++ app.js
@@ -143,6 +143,13 @@ locoScroll.on('scroll', (instance) => {
      pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
   });
 
+///////////// ///////////// ///////////// ///////////// ///////////// ///////////// 
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+///////////// ///////////// ///////////// ///////////// ///////////// ///////////// ///////////// 
 
 
  /* ============================================================================
@@ -231,13 +238,6 @@ function handler2() {
 $(".fs-nav-butt").one("click", handler1);
 
 
-///////////// ///////////// ///////////// ///////////// ///////////// ///////////// 
-  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-  ScrollTrigger.refresh();
-///////////// ///////////// ///////////// ///////////// ///////////// ///////////// ///////////// 
 
 
 
