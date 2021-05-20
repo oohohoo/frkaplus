92674a2dacd9f1c30ba716333c26e984576f4e62
dejan-77 <dejankutic@me.com>
2021-05-21 01:36:47 +0200

    fe

--- app.js
+++ app.js
@@ -691,7 +691,7 @@ function initLoader() {
 
 //.to(lines, {yPercent: -500, stagger: 0.2}, 0)
     .to([loader, loaderContent], {yPercent: -100}, 0.2)
-    .to('#main', {y: 0}, 0);
+    .to('#main', {yPercent: 0}, 0);
 
   const tlLoader = gsap.timeline();
   tlLoader
