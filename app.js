0dae5ef776dc997f5084790eb005da14966f540d
dejan-77 <dejankutic@me.com>
2021-05-20 15:32:30 +0200

    f

--- app.js
+++ app.js
@@ -308,8 +308,8 @@ const showAnim = gsap.timeline({
 
 showAnim
 //.to(".logofade", {autoAlpha:0 }, 0)
-.to(".nav", {yPercent: 50, autoAlpha:0, stagger: 0.05},"<0.1")
-.to(".header-contact--button", {backgroundColor: "hsla(0,4%,13,0)", scale:0.8, x:"3em"},"<0.2")
+.to(".nav", {yPercent: -50, autoAlpha:0, stagger: 0.05},"<0.1")
+.to(".header-contact--button", {scale:0.8, x:"3em"},"<0.2")
 //.to(".kont-color", {color: "black", duration:0.2},"<0.2");
 
 
