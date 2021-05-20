53de3eeeb431604f1688ff3bd1e157eefa8adf2a
dejan-77 <dejankutic@me.com>
2021-05-20 23:01:53 +0200

    gr

--- app.js
+++ app.js
@@ -1360,7 +1360,7 @@ var map = new mapboxgl.Map({
   height: 20, */
 container: 'map', // container ID
 style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [14.6, 16.3], // starting position [lng, lat]
+center: [44.686, 16.342], // starting position [lng, lat]
 zoom: 6.55 // starting zoom
 });
 
