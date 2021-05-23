2564ae3f620ac7ae03a6c1bff7f734a2421175a0
dejan-77 <dejankutic@me.com>
2021-05-23 18:18:48 +0200

    gr

--- app.js
+++ app.js
@@ -1854,20 +1854,20 @@ function saveaspdftest() {
   $('.btn-download').click(function(){
     var doc = new jsPDF("p", "mm", "a4");
     html2canvas(document.querySelector('#demo')).then(function(canvas){
-      var imgData = canvas.toDataURL('image/png');
+      var imgData = canvas.toDataURL('image/jpg');
       var pageHeight = 295;  
       var imgWidth = (canvas.width * 50) / 210 ; 
       var imgHeight = canvas.height * imgWidth / canvas.width;
       var heightLeft = imgHeight;
       var position = 15;
   
-      doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+      doc.addImage(imgData, 'JPG', 0, position, imgWidth, imgHeight);
       heightLeft -= pageHeight;
   
       while (heightLeft >= 0) {
           position = heightLeft - imgHeight;
           doc.addPage();
-          doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+          doc.addImage(imgData, 'JPG', 0, position, imgWidth, imgHeight);
           heightLeft -= pageHeight; 
       }
       doc.output('dataurlnewwindow');
