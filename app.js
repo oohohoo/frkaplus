d7f8fe52198ea7100efaecbaa2d256a811af0929
dejan-77 <dejankutic@me.com>
2021-05-25 09:32:09 +0200

    gr

--- app.js
+++ app.js
@@ -309,7 +309,7 @@ gsap.to(".imagex", {
 
 /*------------/ SCROLLTRIGGER INNER IMAGE PARALLAX /------------*/
 
-gsap.utils.toArray(".img__background").forEach(section => {
+gsap.utils.toArray(".grid").forEach(section => {
 	const pimages = section.querySelectorAll(".img__background");
 
 var inparallax = gsap.timeline({
