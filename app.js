839b7619dfde6c243026314baeeebe8b86bf67ee
dejan-77 <dejankutic@me.com>
2021-05-26 10:40:25 +0200

    fe

--- app.js
+++ app.js
@@ -2037,7 +2037,7 @@ function openTestYourDesign() {
       
 
       .to('#tweenbox', {
-         scaleY: 1,
+         scaleY: 0.8,
           /* rotate:0, */
         duration: 0.64,
         ease: 'power2.inOut',
