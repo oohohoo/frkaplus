5aa9da5006d028866b15c0fd6d1fa6f1ea51077f
dejan-77 <dejankutic@me.com>
2021-05-27 09:37:16 +0200

    th

--- app.js
+++ app.js
@@ -2352,10 +2352,10 @@ function zoomimage() {
 }
 
 
-
+/**/
 function loadimagezoomscript() {
 
-  
+
 let script = document.createElement('script');
 script.src=“https://frkaplus.netlify.app/fullscreenzoom.js”;
 next.container.appendChild(script);
