883d86c464eb94876c17a9f171c9c4dcb5543453
dejan-77 <dejankutic@me.com>
2021-05-27 16:34:39 +0200

    gr

--- app.js
+++ app.js
@@ -879,7 +879,7 @@ views: [{
    
     },
 
-beforeEnter({next}) {
+beforeEnter() {
 
   let script = document.createElement('script');
   script.src = 'https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.js';
