ed68f231f2d1fb918de537c0a0cb434c8183e8fc
dejan-77 <dejankutic@me.com>
2021-05-28 17:55:01 +0200

    gr

--- app.js
+++ app.js
@@ -908,9 +908,10 @@ beforeEnter({next}) {
 
   let script = document.createElement('script');
   script.src = 'https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.js';
-  script.setAttribute('id', 'mapbox-id');
   next.container.appendChild(script); 
-  console.log("MAPBOXX LOADED");
+  
+
+}),
 
 },
 
