efbf05919895d8478d3f8d4a817a6fca7805d7d7
dejan-77 <dejankutic@me.com>
2021-05-28 10:51:38 +0200

    gr

--- app.js
+++ app.js
@@ -797,7 +797,7 @@ function initPageTransitions() {
   barba.hooks.beforeLeave(() => {
    locoScroll.destroy(); 
     
-   current.container.querySelector('mapbox-id').remove();
+   container.querySelector('mapbox-id').remove();
    /*  data.container.removeChild(script);  */
  /*   document.head.remove(script); */
     //data.current.container.parentNode.removeChild(script);
