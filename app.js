b936c2de0d74ab9dd806a907b2e42e94973ef97b
dejan-77 <dejankutic@me.com>
2021-06-03 13:11:49 +0200

    f

--- app.js
+++ app.js
@@ -472,7 +472,7 @@ horizontalSections.forEach(horizontalSection => {
       scroller: ".smooth-scroll",
        scrub: 2,
        trigger: ".horizontal-scroll",
-       pin: ".horizontal-scroll",
+       pin: ".godina--iskustva",
        /* anticipatePin: 1, */
        //markers: true,
        start: "top top",
