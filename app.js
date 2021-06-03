16116fe7da8a890369efb26ca3bd208d3dee4641
dejan-77 <dejankutic@me.com>
2021-06-03 17:57:06 +0200

    gr

--- app.js
+++ app.js
@@ -796,6 +796,8 @@ function initPageTransitions() {
   });
   //init scrolltrigger
   barba.hooks.afterEnter(() => {
+
+    ScrollTrigger.refresh();
    /*
     ScrollTrigger.refresh(true); // ScrollTrigger Refresh
     console.log("scrolltrigger refreshed after enter");
