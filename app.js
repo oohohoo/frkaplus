eb98c4bed99679f0812ffcbad6e2de7450232c42
dejan-77 <dejankutic@me.com>
2021-06-03 13:01:37 +0200

    hu

--- app.js
+++ app.js
@@ -491,14 +491,14 @@ ScrollTrigger.addEventListener("refreshInit", resize);
 console.log("refresh init mamicu mu AJMOOOO");
 */
 ///////////// ///////////// ///////////// ///////////// ///////////// ///////////// 
+ScrollTrigger.addEventListener("refreshInit", resize);
+
   // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
   // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
   ScrollTrigger.refresh();
 ///////////// ///////////// ///////////// ///////////// ///////////// ///////////// ///////////// 
-
-
 
 
 
