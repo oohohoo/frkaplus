77803668b0d5d97398d3af36bebb1ff867588ee8
dejan-77 <dejankutic@me.com>
2021-06-03 13:08:32 +0200

    kl

--- app.js
+++ app.js
@@ -473,6 +473,7 @@ horizontalSections.forEach(horizontalSection => {
        scrub: 2,
        trigger: ".horizontal-scroll",
        pin: "true",
+       anticipatePin: 1,
        //markers: true,
        start: "top top",
        end: () => `+=${pinWrapWidth}`,
