fa39997793b88bd04f27af24e0ea4a9d82649ffe
dejan-77 <dejankutic@me.com>
2021-06-03 12:07:28 +0200

    f

--- app.js
+++ app.js
@@ -332,6 +332,7 @@ inparallax.from(pimages, {
 
  
 /**/
+// Pinning and horizontal scrolling
 
 let horizontalSections = document.querySelectorAll(".horizontal-scroll");
 
@@ -356,6 +357,31 @@ horizontalSections.forEach(horizontalSection => {
 
  });
 
+ /* COLOR CHANGER */
+
+ const scrollColorElems = document.querySelectorAll("[data-bgcolor]");
+ scrollColorElems.forEach((colorSection, i) => {
+   const prevBg = i === 0 ? "" : scrollColorElems[i - 1].dataset.bgcolor;
+   const prevText = i === 0 ? "" : scrollColorElems[i - 1].dataset.textcolor;
+
+   ScrollTrigger.create({
+     trigger: colorSection,
+     scroller: ".smooth-scroll",
+     start: "top 50%",
+     onEnter: () =>
+     gsap.to("body", {
+       backgroundColor: colorSection.dataset.bgcolor,
+       color: colorSection.dataset.textcolor,
+       overwrite: "auto" }),
+
+     onLeaveBack: () =>
+     gsap.to("body", {
+       backgroundColor: prevBg,
+       color: prevText,
+       overwrite: "auto" }) });
+
+
+ });
   /*NE ZNAM DA LI OVO IDE???*/
   //locoScroll.update();
 
