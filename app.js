84fc66bcb3e54ab83e642dc65f4bdf7425550c49
dejan-77 <dejankutic@me.com>
2021-06-08 12:36:09 +0200

    gr

--- app.js
+++ app.js
@@ -2413,7 +2413,8 @@ followArea.forEach(function(el) {
   
   el.addEventListener('mouseover', () => {
     gsap.to(cursor, 0.25, {
-      scale: 8,
+      scale: 3,
+      color: "#ffdc52",
       autoAlpha: 1
     });
 
