58c50adf8c573a6580d6635d11cbc1b4ef29f2d3
dejan-77 <dejankutic@me.com>
2021-06-11 00:28:36 +0200

    gr

--- app.js
+++ app.js
@@ -2113,7 +2113,7 @@ showAnim
 .to(".header-contact--button", {scale:0.7, x:"3.1em"},"<0.2")
 /* .addLabel('revealImage') */
 
-.to(".freveal", {y: 100, duration: 0.3, stagger: 0.02}, "<0.1")
+.to(".freveal", {y: 100, duration: 0.6, stagger: 0.02}, "<0.1")
 .to(".l2", {scaleY: 0, transformOrigin: 'bottom', duration: 0.3}, "<0.2")
 .to(".l3", {scaleX: 0, transformOrigin: 'left', duration: 0.3}, "<0.3")
 .to(".l1", {scaleX: 0, transformOrigin: 'left', duration: 0.3}, "<0.3")
