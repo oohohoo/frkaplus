caa64c1a0e202f91300cd7008e5886155385551c
dejan-77 <dejankutic@me.com>
2021-06-11 02:35:26 +0200

    gr

--- app.js
+++ app.js
@@ -2110,7 +2110,7 @@ const showAnim = gsap.timeline({
 showAnim
 //.to(".logofade", {autoAlpha:0 }, 0)
 .to(".nav", {yPercent: -50, autoAlpha:0, stagger: 0.05},"<0.1")
-.to(".header-contact--button", {scale:0.7, x:"3.1em"},"<0.2")
+.to(".header-contact--button", {scale:0.9, /*x:"3.1em"*/},"<0.2")
 /* .addLabel('revealImage') */
 
 .to(".freveal", {y: 100, duration: 0.6, stagger: 0.02}, "<0.1")
