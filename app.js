af731ba7ca42239ccdfd428bc0a6f88a1e8cb871
dejan-77 <dejankutic@me.com>
2021-06-12 08:55:20 +0200

    gr

--- app.js
+++ app.js
@@ -321,6 +321,7 @@ function initContent() {
     sorting();
     sortButtons();
     customSelect();
+    locationDropdownSelect();
     
     /* appendmore(); */
     console.log("LOKACIJE SCRIPTS -- loaded");
@@ -494,6 +495,7 @@ BARBA VIEWS
                 sorting();
                 sortButtons();
                 stopScrollMap();
+                locationDropdownSelect();
                 console.log("location SORTING pa MAP LOADED + stop scroll map");
               },
               beforeLeave({current}) {
@@ -2659,3 +2661,61 @@ console.log("BOTUNČIĆIIIIIII");
 
 }
 
+
+
+/*
+================================================================================
+CUSTOM SELECT BUTTON - LOKACIJE
+================================================================================
+*/
+
+function locationDropdownSelect() {
+
+
+$('select').each(function(){
+  var $this = $(this), numberOfOptions = $(this).children('option').length;
+
+  $this.addClass('select-hidden'); 
+  $this.wrap('<div class="select"></div>');
+  $this.after('<div class="select-styled"></div>');
+
+  var $styledSelect = $this.next('div.select-styled');
+  $styledSelect.text($this.children('option').eq(0).text());
+
+  var $list = $('<ul />', {
+      'class': 'select-options'
+  }).insertAfter($styledSelect);
+
+  for (var i = 0; i < numberOfOptions; i++) {
+      $('<li />', {
+          text: $this.children('option').eq(i).text(),
+          rel: $this.children('option').eq(i).val()
+      }).appendTo($list);
+  }
+
+  var $listItems = $list.children('li');
+
+  $styledSelect.click(function(e) {
+      e.stopPropagation();
+      $('div.select-styled.active').not(this).each(function(){
+          $(this).removeClass('active').next('ul.select-options').hide();
+      });
+      $(this).toggleClass('active').next('ul.select-options').toggle();
+  });
+
+  $listItems.click(function(e) {
+      e.stopPropagation();
+      $styledSelect.text($(this).text()).removeClass('active');
+      $this.val($(this).attr('rel'));
+      $list.hide();
+      //console.log($this.val());
+  });
+
+  $(document).click(function() {
+      $styledSelect.removeClass('active');
+      $list.hide();
+  });
+
+});
+
+}
