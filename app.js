c20288725092f39456c8e355c167ecac6ee0f348
dejan-77 <dejankutic@me.com>
2021-06-14 16:44:05 +0200

    gr

--- app.js
+++ app.js
@@ -3141,11 +3141,7 @@ Demo.prototype.filter = function () {
   
 } else{
 this.message.innerHTML = "";
-setTimeout(()=>{
-  locoScroll.update();
-  ScrollTrigger.refresh(true);
-   console.log("Locoscrollupdated + SCROLLTRIGGER REFRESHHHHH 4");
-    },200)  
+
 
  }
 
