f2cab6bf3f813d5eeaaeb9f570db7e44fa35d53b
dejan-77 <dejankutic@me.com>
2021-06-15 21:22:46 +0200

    fe

--- app.js
+++ app.js
@@ -743,7 +743,7 @@ map.on('click', function(e) {
     var popup = new mapboxgl.Popup({ offset: [0, -15] })
     .setLngLat(feature.geometry.coordinates)
     .setHTML(
-      '<h3>' + feature.properties.title + '</h3>' +
+      '<p>' + feature.properties.title + '<p>' +
       '<p>' + feature.properties.description + '</p>'
       )
     .addTo(map);
