4d75c3996186e9a39199ffce24b34ebc1ae50d4d
dejan-77 <dejankutic@me.com>
2021-06-16 16:37:23 +0200

    f

--- app.js
+++ app.js
@@ -3135,13 +3135,13 @@ var Demo = function (element) {
   this.message = document.querySelector('.js-message');
 
 	this.shuffle = new Shuffle(element, {
-    buffer: 1,
+  //  buffer: 0,
 		easing: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)', // easeOutQuart
 		sizer: '.the-sizer',
-    staggerAmount: 15, // Transition delay offset for each item in milliseconds.
+   /* staggerAmount: 15, // Transition delay offset for each item in milliseconds.
     staggerAmountMax: 150, // Maximum stagger delay in milliseconds.
     throttle: throttle, // By default, shuffle will throttle resize events. This can be changed or removed.
-    throttleTime: 300,
+    throttleTime: 300,*/
 
 	});
 
