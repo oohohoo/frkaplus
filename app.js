cc050976f129c3d706a1d7bb1d580c0b941fc73b
dejan-77 <dejankutic@me.com>
2021-06-17 14:58:22 +0200

    gr

--- app.js
+++ app.js
@@ -2763,7 +2763,7 @@ this.regions = Array.from(document.querySelectorAll('.js-regions input'));
 		easing: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)', // easeOutQuart
 		/* sizer: '.sizer', */
     buffer:1,
-    gutterWidth: 20,
+    gutterWidth: 30,
     roundTransforms: true,
     useTransforms: true,
     itemSelector: '.webinar-posts__event-item',
