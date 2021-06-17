0e4f8c75b74f255e9638f80a39f5f8279b2cd29b
dejan-77 <dejankutic@me.com>
2021-06-17 16:39:07 +0200

    g

--- app.js
+++ app.js
@@ -2775,7 +2775,7 @@ this.regions = Array.from(document.querySelectorAll('.js-regions input'));
 	});
 
 
-  $(function() {
+  $(document).ready(function(){
   $('.location-switcher_wrap').click(function() {
     /*   $('.webinar-posts__event-item').toggleClass('full');
       $('.webinar-posts__event-item').toggleClass('full'); 
@@ -2788,7 +2788,8 @@ this.regions = Array.from(document.querySelectorAll('.js-regions input'));
       console.log("SHUFFLE APDEJT FROM FUNCTION");
     });
 
-  });
+  }
+
   this.shuffle.layout();
   console.log("SHUFFLE LAYOUT");
   
