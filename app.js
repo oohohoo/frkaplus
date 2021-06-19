e658b7957e3dd979823f92cd9e0e3a3c2f43f703
dejan-77 <dejankutic@me.com>
2021-06-19 16:17:04 +0200

    ht

--- app.js
+++ app.js
@@ -723,7 +723,7 @@ Add an event listener that runs
   when a user clicks on the map element.
 */
 
-map.addSource('frka', {
+map.addSource('frkax', {
   type: 'geojson',
   data: 'https://frkaplus.netlify.app/frka.geojson'
 });
