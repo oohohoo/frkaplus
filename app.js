e3bdd70cb9d667f01b7e43ffa1bc6a094adb7a41
dejan-77 <dejankutic@me.com>
2021-06-19 08:58:54 +0200

    gr

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ function initScroll(container) {
     offset: [0, 0],
     getDirection: true,
     scrollbarContainer: true,
-    scrollbarContainer: document.querySelector('#main'),
+    scrollbarContainer: document.querySelector('.smooth-scroll'),
     scrollFromAnywhere: true,
     multiplier: 1.0, // body sroll speed / object speed values are multiplied
     touchMultiplier: 3.0,
