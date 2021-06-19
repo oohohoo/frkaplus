b7a9fd248c4d18be46cbbfcc1cddcad6b9f791de
dejan-77 <dejankutic@me.com>
2021-06-19 13:34:51 +0200

    f

--- app.js
+++ app.js
@@ -744,8 +744,8 @@ map.on('click', function(e) {
     .setLngLat(feature.geometry.coordinates)
      .setHTML(
       '<h3>' + feature.properties.title + '</h3>' +
-      '<p>' + feature.properties.description + '</p>' +
-      '<p><img src="'feature.properties.image'"></p>')
+      '<p>' + feature.properties.description + '</p>' 
+     /*  '<p><img src="'feature.properties.image'"></p>') */
       )
       /* .setHTML("<h3>Hello World!</h3>") */
       /* .setMaxWidth("300px") */
