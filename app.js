b67d902ef7775d94ec810ba6934369eee21a8e6a
dejan-77 <dejankutic@me.com>
2021-06-22 20:44:43 +0200

    gr

--- app.js
+++ app.js
@@ -2234,7 +2234,7 @@ $(document).ready(function()
 
   $('#removevideo').click(function(){
     //  var toAdd = $('input[name=checkListItem]').val();
-      var item = $('video');
+      var item = $('.filepond--media-preview video');
       $('.mobile').remove(item);
       console.log("VIDEO ODEEE");
 });
