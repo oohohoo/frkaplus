a6f28ab078e0da381f5747cf07923c33dffda8fb
dejan-77 <dejankutic@me.com>
2021-06-22 20:19:30 +0200

    gr

--- app.js
+++ app.js
@@ -2234,9 +2234,9 @@ $(document).ready(function()
 
   $('#removevideo').click(function(){
     //  var toAdd = $('input[name=checkListItem]').val();
-      var item = $('.filepond--media-preview video');
+      var item = $('.filepond--media-preview');
       $('.mobile').prepend(item);
-      console.log("VIDEO REMOVED");
+      console.log("VIDEO REMOVEDDD");
 });
 
 
