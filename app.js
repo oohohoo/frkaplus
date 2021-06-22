14336538cf8b747166327f276a30b61255a35937
dejan-77 <dejankutic@me.com>
2021-06-22 08:54:58 +0200

    gr

--- app.js
+++ app.js
@@ -636,7 +636,9 @@ function locationMap() {
     zoom: 6.64, // starting zoom
     minZoom: 6.64, // note the camel-case
     maxZoom: 19,
-    maxBounds: bounds
+    maxBounds: bounds,
+    bearing: 27,
+    pitch: 45
   });
 
 
