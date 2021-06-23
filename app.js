2a7273f04b8d39d798016eef08fe3edcbcd6ce57
dejan-77 <dejankutic@me.com>
2021-06-23 18:38:58 +0200

    gr

--- app.js
+++ app.js
@@ -1441,22 +1441,45 @@ SPLIDE
 function splidesolo() {
 
   const splide = new Splide(".splide", {
-      perPage: 1,
-      perMove: 1,
-      type: 'loop',
-      //focus: 'center',
-      arrows: 'slider',
-      easing: 'cubic-bezier(.42,.65,.27,.99)',
-      pagination: 'slider',
-      //gap: '2em', 
-      updateOnMove : true,
-      breakpoints: {
-        767: {
-          perPage: 1,
-        }
-      }
-    })
-    .mount();
+       // Desktop on down
+	perPage: 3,
+	perMove: 1,
+  focus: 0, // 0 = left and 'center' = center
+  type: 'slide', // 'loop' or 'slide'
+  gap: '2em', // space between slides
+  arrows: 'slider', // 'slider' or false
+  pagination: 'slider', // 'slider' or false
+  speed : 600, // transition speed in miliseconds
+  dragAngleThreshold: 60, // default is 30
+  autoWidth: false, // for cards with differing widths
+  rewind : false, // go back to beginning when reach end
+  rewindSpeed : 400,
+  waitForTransition : false,
+  updateOnMove : true,
+  trimSpace: false, // true removes empty space from end of list
+  breakpoints: {
+		991: {
+    	// Tablet
+			perPage: 2,
+      gap: '3vw',
+		},
+    767: {
+    	// Mobile Landscape
+			perPage: 1,
+      gap: '3vw',
+		},
+    479: {
+    	// Mobile Portrait
+			perPage: 1,
+      gap: '3vw',
+		}
+	}
+} ).mount();
+}
+
+}
+slider1();
+
 /*
   $('.next-splide').click(function () {
     $('.splide__arrow.splide__arrow--next').click();
