2a6793c4bb7160f3c0fde3bbaf2488cfaf16528b
dejan-77 <dejankutic@me.com>
2021-06-28 19:20:54 +0200

    ht

--- app.js
+++ app.js
@@ -145,7 +145,8 @@ function initScroll(container) {
 // CLOSE DROPDOWN ON SCROLL
 
 locoScroll.on('scroll', (instance) => {
-  $(".select2-container.select2-dropdown-open").select2('close');
+ // $(".select2-container.select2-dropdown-open").select2('close');
+  $("#select2_element").select2("close"); }, true)
  // $('.webinar-posts__event-item').toggleClass('full');
    // $(".select2-container.select2-dropdown-open").not($(this)).select2('positionDropdown');
   console.log("Dropdown AJ");
