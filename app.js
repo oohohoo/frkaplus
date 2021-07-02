93c63bb78d364f5c8257e023a2655dac70fb4548
dejan-77 <dejankutic@me.com>
2021-07-02 17:01:36 +0200

    gr

--- app.js
+++ app.js
@@ -294,11 +294,16 @@ function initContent() {
  // splidesolo();
   simulatorButton();
   console.log("initial scripts loaded...");
+  ScrollTrigger.refresh(true); // ScrollTrigger Refresh
+  console.log("scrolltrigger refreshed script load");
+ 
  
   if (document.querySelector('#home')) {
     homevideo();
     openMobileMenu();
     console.log("home scripts loaded...");
+    ScrollTrigger.refresh(true); // ScrollTrigger Refresh
+    console.log("scrolltrigger refreshed script load");
   }
 
   if (document.querySelector('#onama')) {
@@ -310,6 +315,8 @@ function initContent() {
     smallCounteronama();
     openMobileMenu();
     console.log("o nama scripts loaded...");
+    ScrollTrigger.refresh(true); // ScrollTrigger Refresh
+    console.log("scrolltrigger refreshed script load");
 
   }
   if (document.querySelector('#lokacije')) {
@@ -320,6 +327,8 @@ function initContent() {
     stopScrollMap();
     openMobileMenu();
     console.log("lokacije scripts loaded...");
+    ScrollTrigger.refresh(true); // ScrollTrigger Refresh
+    console.log("scrolltrigger refreshed script load");
   }
 
   if (document.querySelector('#lokacijesolo')) {
@@ -335,11 +344,15 @@ function initContent() {
     stopScrollMap();
     splidesolo();
     console.log("lokacijesolo scripts loaded...");
+    ScrollTrigger.refresh(true); // ScrollTrigger Refresh
+    console.log("scrolltrigger refreshed script load");
   }
 
   if (document.querySelector('#kontakt')) {
       openMobileMenu();
     console.log("kontakt scripts loaded...");
+    ScrollTrigger.refresh(true); // ScrollTrigger Refresh
+    console.log("scrolltrigger refreshed script load");
   }
 
 //DRUKČIJE SE PONAŠA AKO UBACIMO UNUTAR document ready/ 
@@ -502,7 +515,6 @@ barba.init({
               beforeEnter(data) {
                 soloMap();
                 splidesolo();
-                scrolltriggerupdate();
                 openTestYourDesign();
                 uploadCrop();
                 saveaspdf();
