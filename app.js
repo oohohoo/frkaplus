c700c000b1ddf3a2dea1f097c52bcc4f3e3a3873
dejan-77 <dejankutic@me.com>
2021-07-05 18:13:56 +0200

    f

--- app.js
+++ app.js
@@ -3413,7 +3413,7 @@ PIN HOVER
 */
 
 function pinHover() {
-  gsap.utils.toArray(".lokacije-small-header").forEach(container => {
+  gsap.utils.toArray(".pin-button-link").forEach(container => {
     let onicon = container.querySelector(".pinicon"),
      officon = container.querySelector(".piniconblack"),
       pinhover = gsap.timeline({
