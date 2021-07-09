4924220b1795938655e3572cb91fd7afbb28e366
dejan-77 <dejankutic@me.com>
2021-07-09 12:40:14 +0200

    fe

--- app.js
+++ app.js
@@ -347,7 +347,7 @@ function initContent() {
     openMobileMenu();
     soloMap();
     stopScrollMap();
-    //splidesolo();
+    splidesolo();
     console.log("lokacijesolo scripts loaded...");
    // ScrollTrigger.refresh(true); // ScrollTrigger Refresh
     //console.log("scrolltrigger refreshed script load");
