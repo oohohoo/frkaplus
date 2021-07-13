357469d7edf06ce0c58ad8f51062e5b50d809c81
dejan-77 <dejankutic@me.com>
2021-07-13 19:48:59 +0200

    gr

--- app.js
+++ app.js
@@ -3387,12 +3387,7 @@ function doCoolStuff() {
 }
 
 
-window.addEventListener('resize', function(){
- 
-  animation.to(".slider", {x:targets[activeTab].offsetLeft, width:targets[activeTab].offsetWidth});
 
-  console.log("RESIZE RESSSIIIZZZEEE");
-});
 
 
 }
