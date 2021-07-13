9ed67c418cbada245fc197c9142ef613961a735a
dejan-77 <dejankutic@me.com>
2021-07-13 10:58:24 +0200

    gr

--- app.js
+++ app.js
@@ -1020,7 +1020,7 @@ var fadein = gsap.timeline({defaults:{ease:'none'}})
 .to('.loader-icon', {autoAlpha:1})
 .to('.loader-wrap', {rotate: 360, repeat: -1, duration:1}, "<")
 .to('.loader-txt', {autoAlpha:1, repeat: -1, duration:0.5}, "<")
-.to('.lokacije-mapbox-container', {autoAlpha:1, duration:1.5, delay:0.5})
+.to('.static', {autoAlpha:0, duration:1.5, delay:0.5})
 .to('.loader-icon', {autoAlpha:0}, "-=1.5")
 .to('.loader-txt', {autoAlpha:0}, "<")
 
