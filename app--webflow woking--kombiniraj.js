a735b8d4e30f71b4d4a26660b66eb87051e0f59e
dejan-77 <dejankutic@me.com>
2021-07-13 11:05:20 +0200

    gr

--- app--webflow woking--kombiniraj.js
+++ app--webflow woking--kombiniraj.js
@@ -879,6 +879,10 @@ console.log("MAP RESIZE!");
 /* mapboxgl.clearStorage();
 console.log("STORAGE CLEARED");  */
 
+map.on("load", function (e) {
+  const mapContainerEl = document.getElementById("map");
+  mapContainerEl.style.visibility = "visible";
+});
 
 /*
 ================================================================================
