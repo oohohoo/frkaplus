69571407e3fa3091e087a6906bca19b599a409bd
dejan-77 <dejankutic@me.com>
2021-07-15 15:56:50 +0200

    ji

--- app.js
+++ app.js
@@ -1951,7 +1951,7 @@ function horizpin() {
         pin: ".stophor",
         /* anticipatePin: 1, */
         //markers: true,
-        start: "top 10%",
+        start: "top 8%",
         end: () => `+=${pinWrapWidth}`,
         invalidateOnRefresh: true
       },
