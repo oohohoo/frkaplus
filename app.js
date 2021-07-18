659d728e0e0df0f3510741c6ede9e13750db5ba9
dejan-77 <dejankutic@me.com>
2021-07-18 10:30:02 +0200

    r

--- app.js
+++ app.js
@@ -957,7 +957,7 @@ FADE IN MAP
  
 var fadein = gsap.timeline({defaults:{ease:'none'}})
 .to('.loader-icon', {autoAlpha:1})
-.to('.loader-wrap', {rotate: 360, repeat: -1, duration:3}, "<")
+.to('.loader-wrap', {rotate: 360, repeat: -1, duration:5}, "<")
 //.to('.loader-txt', {autoAlpha:1, repeat: -1, duration:0.5}, "<")
 //.to('.static', {autoAlpha:0, duration:1.5, delay:0.5})
 .to('.loader-icon', {autoAlpha:0}, "-=1.5")
@@ -983,7 +983,7 @@ var fadein = gsap.timeline({defaults:{ease:'none'}})
 
 /*
 ================================================================================
-LOCATION MAP
+SOLOMAP MAP
 ================================================================================
 */
 function soloMap() {
