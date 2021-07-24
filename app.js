d045d97983af1bd140c769bb7689420ffce9371e
dejan-77 <dejankutic@me.com>
2021-07-24 14:12:23 +0200

    ge

--- app.js
+++ app.js
@@ -2937,12 +2937,12 @@ SHUFFLE.JS - SWITCH LAYOUT LOKACIJE ITEMS
 ================================================================================
 */
 Demo.prototype._handleupdate = function (evt) {
-  /* setTimeout(() => { */
+  setTimeout(() => {
     this.shuffle.layout();
     locoScroll.update();
     ScrollTrigger.refresh(true);
     console.log("Locoscrollupdated + SCROLLTRIGGER NUOVO");
- /*  }, 1) */
+  }, 1)
 };
 
 /*
