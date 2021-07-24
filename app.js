ae2ac5fb8c23aaa5d395755dbc009dedca1edc95
dejan-77 <dejankutic@me.com>
2021-07-24 22:45:51 +0200

    gr

--- app.js
+++ app.js
@@ -454,6 +454,17 @@ function initPageTransitions() {
    
   });
 
+  barba.hooks.after((data) => {
+    let js = data.next.container.querySelectorAll('main script');
+    if(js != null){
+            js.forEach((item) => {
+                console.log(js)
+                eval(item.innerHTML);
+            });
+    }
+
+    console.log("SCRIPTS EVALUATED NO PROBLEM");
+});
   // scroll to the top of the page
 /*   barba.hooks.enter(() => {
     window.scrollTo(0, 0);
@@ -461,18 +472,18 @@ function initPageTransitions() {
   }); */
   barba.hooks.afterLeave((data) => {
     // Set <body> classes for "next" page
-  //  var nextHtml = data.next.html;
-   // var response = nextHtml.replace(/(<\/?)body( .+?)?>/gi, '$1notbody$2>', nextHtml)
-  //  var bodyClasses = $(response).filter('notbody').attr('class')
-   // $("body").attr("class", bodyClasses);
+    var nextHtml = data.next.html;
+    var response = nextHtml.replace(/(<\/?)body( .+?)?>/gi, '$1notbody$2>', nextHtml)
+    var bodyClasses = $(response).filter('notbody').attr('class')
+    $("body").attr("class", bodyClasses);
     
-   // console.log("BODY CLASSES UPDATED");
+    console.log("BODY CLASSES UPDATED");
   });
   
   barba.hooks.enter((data) => {
     window.scrollTo(0, 0);
-   // let parser = new DOMParser();
-    //let dom = parser.parseFromString(data.next.html, 'text/html');
+    let parser = new DOMParser();
+    let dom = parser.parseFromString(data.next.html, 'text/html');
     //let nextHeader = parser.parseFromString(data);
     //document.querySelector('.header').innerHTML = nextHeader.innerHTML;
      });
@@ -511,7 +522,7 @@ function initPageTransitions() {
   BARBA PREFETCH
   ================================================================================
   */
-  barba.use(barbaPrefetch);
+  //barba.use(barbaPrefetch);
 
   /*
   ================================================================================
