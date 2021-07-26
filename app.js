0c39fa921af27acc2e85acaafc5f59644c52e565
dejan-77 <dejankutic@me.com>
2021-07-26 16:57:52 +0200

    grr

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ MAIN JS + LOCOMOTIVE SCROLL + SCROLL TRIGGER
 
 function initScroll(container) {
 
-  const locoScroll = new LocomotiveScroll({
+  locoScroll = new LocomotiveScroll({
     el: document.querySelector(".smooth-scroll"),
     smooth: true, 
     /* offset: [0, 0], */
@@ -118,21 +118,20 @@ function initScroll(container) {
 
   // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
   locoScroll.on("scroll", ScrollTrigger.update);
-  /* locoScroll.on("scroll", () => ScrollTrigger.update()); */
-  console.log("LOCO UPDATED AJDEEE JEBEMTE");
+
   // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
   ScrollTrigger.scrollerProxy(".smooth-scroll", {
     scrollTop(value) {
       return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
     }, // we don't have to define a scrollLeft because we're only scrolling vertically.
     getBoundingClientRect() {
-      return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+      return {top: 0, left: 0,width: window.innerWidth,height: window.innerHeight};
     },
 
     // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, 
     // we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
     /*OVO NE RADI SA DATA_SCROLL_SECTION */
-   pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"  
+    pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"  
   });
 
   // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
@@ -295,7 +294,7 @@ function initContent() {
             this.onama();
         } 
         else if ($('body').hasClass('page-template-lokacije')) {
-         this.lokacije();
+          this.lokacije();
         }
         else if ($('body').hasClass('single-lokacije')) {
           this.sololokacije();
