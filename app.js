f0af17d51deda1d9665a060adc692bad838f46a5
dejan-77 <dejankutic@me.com>
2021-07-26 09:06:48 +0200

    grgr

--- app.js
+++ app.js
@@ -455,7 +455,7 @@ function initPageTransitions() {
    
   });
 
-/*   barba.hooks.after((data) => {
+  barba.hooks.after((data) => {
     let js = data.next.container.querySelectorAll('main script');
     if(js != null){
             js.forEach((item) => {
@@ -465,7 +465,7 @@ function initPageTransitions() {
     }
 
     console.log("SCRIPTS EVALUATED NO PROBLEM");
-}); */
+});
   // scroll to the top of the page
 /*   barba.hooks.enter(() => {
     window.scrollTo(0, 0);
@@ -473,18 +473,18 @@ function initPageTransitions() {
   }); */
   barba.hooks.afterLeave((data) => {
     // Set <body> classes for "next" page
-    var nextHtml = data.next.html;
+  /*   var nextHtml = data.next.html;
     var response = nextHtml.replace(/(<\/?)body( .+?)?>/gi, '$1notbody$2>', nextHtml)
     var bodyClasses = $(response).filter('notbody').attr('class')
     $("body").attr("class", bodyClasses);
     
-    console.log("BODY CLASSES UPDATED");
+    console.log("BODY CLASSES UPDATED"); */
   });
   
   barba.hooks.enter((data) => {
     window.scrollTo(0, 0);
-    let parser = new DOMParser();
-    let dom = parser.parseFromString(data.next.html, 'text/html');
+   /*  let parser = new DOMParser();
+    let dom = parser.parseFromString(data.next.html, 'text/html'); */
     //let nextHeader = parser.parseFromString(data);
     //document.querySelector('.header').innerHTML = nextHeader.innerHTML;
      });
