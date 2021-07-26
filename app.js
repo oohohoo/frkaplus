bf4c5ce8761ef261fdcd9c4ad3deedb62844253e
dejan-77 <dejankutic@me.com>
2021-07-26 08:53:19 +0200

    gr

--- app.js
+++ app.js
@@ -281,7 +281,7 @@ INIT CONTENT --> vodi na --> INIT SCROLL
 */
 function initContent() {
 
-  select('body').classList.remove('is-loading');
+/*   select('body').classList.remove('is-loading'); */
 
  // (function($) {
 
