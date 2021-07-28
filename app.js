d35d1a309336903e340d8514fb7d0f874366b191
dejan-77 <dejankutic@me.com>
2021-07-28 17:34:34 +0200

    ht

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ locoScroll = new LocomotiveScroll({
     // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, 
     // we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
     /*OVO NE RADI SA DATA_SCROLL_SECTION */
-   pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"  
+   //pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"  
   });
 
   // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
