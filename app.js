94aa983e59f762d51d4c09ac1749639d6711883d
dejan-77 <dejankutic@me.com>
2021-07-29 17:58:54 +0200

    f

--- app.js
+++ app.js
@@ -2741,8 +2741,7 @@ $('#crop_img').click(function () {
 
 /* VIDEO BUTTOINs*/
 $('.upload-video').on('FilePond:addfile', function () {
-  crop_preview_cont.slideUp();
-  $('.movevideo').removeClass('hide');
+    $('.movevideo').removeClass('hide');
 });
 
 /* VIDEO BUTTOINs*/
