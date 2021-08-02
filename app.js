e5da3cc1e76c65525acae98b20c2dff9580e730e
dejan-77 <dejankutic@me.com>
2021-08-02 18:30:06 +0200

    gr

--- app.js
+++ app.js
@@ -2011,8 +2011,8 @@ function numberoll() {
           end: "bottom top",
           scrub: 1,
         }
-        })
-        tl.to(".roll-number-wrap" {y: -700, overwrite: "auto"});
+        });
+        tl.to(".roll-number-wrap", {y: -700, overwrite: "auto"});
  
        }, 
 
@@ -2028,8 +2028,8 @@ function numberoll() {
           end: "bottom top",
           scrub: 1,
         }
-        })
-        tl.to(".roll-number-wrap" {y: -300, overwrite: "auto"});
+        });
+        tl.to(".roll-number-wrap", {y: -300, overwrite: "auto"});
    }, 
     
     // all 
@@ -2041,6 +2041,8 @@ function numberoll() {
    });
 
 
+
+   
 
  
 
