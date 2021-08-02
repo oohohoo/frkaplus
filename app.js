be3380611a85d8ab14e582caba55ab294e954d9e
dejan-77 <dejankutic@me.com>
2021-08-02 16:02:41 +0200

    gr

--- app.js
+++ app.js
@@ -290,10 +290,9 @@ function initLoader() {
     //.set("#main", {y: 80})
 
     /* OVO JE DODANO ZA PREZENTACIJU*/
-   /*  .set(progress, {autoAlpha: 0}) */
-   /* .to(loaderInner, {scaleY: 1, transformOrigin: 'bottom', ease: 'power1.inOut'}) */
-    .to(loaderInner, {scaleX: 1,transformOrigin: 'left bottom',ease: 'power1.inOut'}, "<") 
-   .to(loaderInner, {scaleX: 0,transformOrigin: 'right bottom',ease: 'power1.inOut'}) 
+
+   /*  .to(loaderInner, {scaleX: 1,transformOrigin: 'left bottom',ease: 'power1.inOut'}, "<") 
+   .to(loaderInner, {scaleX: 0,transformOrigin: 'right bottom',ease: 'power1.inOut'})  */
 
     .addLabel('revealImage')
     .to(".l1", {scaleX: 1, transformOrigin: 'left', duration: 0.6}, 'revealImage+=0.1')
