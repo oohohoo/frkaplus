94b9ac5163966ea35a29cb9735f8bcb6a36a17fa
dejan-77 <dejankutic@me.com>
2021-08-02 21:58:47 +0200

    gr

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ locoScroll = new LocomotiveScroll({
     multiplier: 1.0, // body sroll speed / object speed values are multiplied
     touchMultiplier: 3.0,
     useKeyboard: true,
-     inertia: .6,  
+     inertia: .75,  
     smartphone: {
       smooth: true,
       
