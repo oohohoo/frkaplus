f97fc14a6707f8bde612a04d1cff36769e4d4978
dejan-77 <dejankutic@me.com>
2021-08-04 18:14:04 +0200

    ht

--- app.js
+++ app.js
@@ -1899,7 +1899,7 @@ function homevideo() {
          scrollTrigger: {
             scroller: ".smooth-scroll",
             trigger: apelTrigger,
-            start: "top 70%",
+            start: "top 50%",
             toggleActions: "restart none none reverse",  
           },
         });
