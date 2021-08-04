82bb74d7490f459f44e635df4fef16efef6d5635
dejan-77 <dejankutic@me.com>
2021-08-04 17:46:08 +0200

    gr

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ locoScroll = new LocomotiveScroll({
       smooth: false,
     },
     tablet: {
-      smooth: true,
+      smooth: false,
     } 
   });
 // data direction ubačeno naknadno
@@ -250,7 +250,7 @@ RELOAD ON MOBILE ORIENTATION CHANGE // OVO TESTIRAJ DA LI TREBA
   w.addEventListener( "devicemotion", checkTilt, false );
 
 })( this ); */
-}
+/* } */
 
 /*
 ================================================================================
@@ -289,8 +289,8 @@ function initLoader() {
     /* OVO JE DODANO ZA PREZENTACIJU*/
    /*  .set(progress, {autoAlpha: 0}) */
    /* .to(loaderInner, {scaleY: 1, transformOrigin: 'bottom', ease: 'power1.inOut'}) */
-  /*   .to(loaderInner, {scaleX: 1,transformOrigin: 'left bottom',ease: 'power1.inOut'}, "<") 
-   .to(loaderInner, {scaleX: 0,transformOrigin: 'right bottom',ease: 'power1.inOut'})  */
+   .to(loaderInner, {scaleX: 1,transformOrigin: 'left bottom',ease: 'power1.inOut'}, "<") 
+   .to(loaderInner, {scaleX: 0,transformOrigin: 'right bottom',ease: 'power1.inOut'})  
 
     .addLabel('revealImage')
     .to(".l1", {scaleX: 1, transformOrigin: 'left', duration: 0.6}, 'revealImage+=0.1')
