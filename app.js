785449530c2dd51ae06dc5993e4fe78ff419894e
dejan-77 <dejankutic@me.com>
2021-09-14 16:47:32 +0200

    gr

--- app.js
+++ app.js
@@ -530,6 +530,12 @@ function initPageTransitions() {
 
   //  console.log("SCRIPTS EVALUATED NO PROBLEM");
 });
+
+barba.hooks.afterEnter((data) => {
+  // this hook will be called for each transitions
+  //data.current.url is the page path.
+  gtag('config', 'UA-207549905-1', {'page_path': data.current.url});
+});
   // scroll to the top of the page
   barba.hooks.enter(() => {
     window.scrollTo(0, 0);
