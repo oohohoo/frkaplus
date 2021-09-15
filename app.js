09f581d77000beab2c2d87145daaeba0575b848e
dejan-77 <dejankutic@me.com>
2021-09-15 19:47:22 +0200

    f

--- app.js
+++ app.js
@@ -1787,7 +1787,7 @@ function outlinehero() {
     scrollTrigger:{
     scroller: ".smooth-scroll",
       trigger: ".img__wrapper--hero", 
-      start: "top 15%", 
+      start: "top 17%", 
       end: "bottom top", 
       scrub: 1,
       markers: true, 
@@ -1800,7 +1800,7 @@ function outlinehero() {
     scrollTrigger:{
     scroller: ".smooth-scroll",
       trigger: ".img__wrapper--hero", 
-      start: "top 15%", 
+      start: "top 17%", 
       end: "bottom top", 
       scrub: 1
     },
