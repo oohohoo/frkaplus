f1b3911a81d51591dbe1960cd1b3d4adb4b72da6
dejan-77 <dejankutic@me.com>
2021-09-15 19:28:37 +0200

    f

--- app.js
+++ app.js
@@ -1782,7 +1782,7 @@ O NAMA // OUTLINE HERO
 */
 
 function outlinehero() {
-  /*  gsap.set('.filled-text, .outline-text', {x:-500}); */
+    gsap.set('.filled-text, .outline-text', {x:-500}); 
   gsap.to(".filled-text, .outline-text", {
     scrollTrigger:{
     scroller: ".smooth-scroll",
@@ -1794,7 +1794,7 @@ function outlinehero() {
     x: 500
   });
   
-  /*  gsap.set('.filledtwo, .outlinetwo', {x:500});  */
+    gsap.set('.filledtwo, .outlinetwo', {x:500});  
   gsap.to(".filledtwo, .outlinetwo", {
     scrollTrigger:{
     scroller: ".smooth-scroll",
