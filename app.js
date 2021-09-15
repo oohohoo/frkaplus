f7c270903e73415e3c6ff49149d9b49b74746307
dejan-77 <dejankutic@me.com>
2021-09-15 17:34:20 +0200

    gr

--- app.js
+++ app.js
@@ -1873,6 +1873,16 @@ function zoomMobile() {
 
     "(max-width: 991px)": function() {
 
+      gsap.to("[data-speed]", {
+        y: (i, el) => (1 - parseFloat(el.getAttribute("data-speed"))) * ScrollTrigger.maxScroll(window) ,
+        ease: "none",
+        scrollTrigger: {
+          start: 0,
+          end: "max",
+          invalidateOnRefresh: true,
+          scrub: 0
+        }
+      });
 
       /*
       gsap.to(".zz1", {
