80aaf054912465a2f02ccffc25b2843a19fab042
dejan-77 <dejankutic@me.com>
2021-09-16 12:17:49 +0200

    gr

--- app.js
+++ app.js
@@ -859,10 +859,9 @@ FADE IN MAP
 */
 gsap.set('.lokacije-body', {opacity:0})
 var fadein = gsap.timeline({defaults:{ease:'none'}})
-.to('.lokacije-body', {opacity:1, duration:0.5})
 .to('.loader-icon', {autoAlpha:1})
 .to('.loader-wrap', {rotate: 360, repeat: -1, duration:5}, "<")
-
+.to('.lokacije-body', {opacity:1, duration:3},"<")
 //.to('.static', {autoAlpha:0, duration:1.5, delay:0.5})
 .to('.loader-icon', {autoAlpha:0}, "-=1.5")
 //.to('.loader-txt', {autoAlpha:0}, "<")
