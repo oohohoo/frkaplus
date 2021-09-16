1878e02482eaf89461614464f8a3caf96b32d5fe
dejan-77 <dejankutic@me.com>
2021-09-16 11:37:34 +0200

    ht

--- app.js
+++ app.js
@@ -1042,11 +1042,11 @@ var fadein = gsap.timeline({defaults:{ease:'none'}})
 //.to('.static', {autoAlpha:0, duration:1.5, delay:0.5})
 .to('.loader-icon', {autoAlpha:0}, "-=1.5")
 //.to('.loader-txt', {autoAlpha:0}, "<")
-}
+}); 
 /*ADD DATA ATTRIBUTE TO MAPBOX LINKS*/
 /*   document.querySelectorAll( 'a[href^="http"]:not([href*="example.com"])' ).forEach(anchor => anchor.setAttribute( 'rel', 'noopener noreferrer nofollow' )); 
  */
-}); 
+
 
   
 /*
