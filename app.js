9e7866c46764b25fd136175612e582e5eeda08aa
dejan-77 <dejankutic@me.com>
2021-09-16 11:40:22 +0200

    gr

--- app.js
+++ app.js
@@ -1033,7 +1033,7 @@ map.on("load", function (e) {
 FADE IN MAP
 ================================================================================
 */
-$(document).ready(function(){ 
+.set('.lokacije-body', {autoAlpha:0})
 var fadein = gsap.timeline({defaults:{ease:'none'}})
 .to('.lokacije-body', {autoAlpha:1, duration:0.5})
 .to('.loader-icon', {autoAlpha:1})
@@ -1042,7 +1042,7 @@ var fadein = gsap.timeline({defaults:{ease:'none'}})
 //.to('.static', {autoAlpha:0, duration:1.5, delay:0.5})
 .to('.loader-icon', {autoAlpha:0}, "-=1.5")
 //.to('.loader-txt', {autoAlpha:0}, "<")
-}); 
+//}); 
 /*ADD DATA ATTRIBUTE TO MAPBOX LINKS*/
 /*   document.querySelectorAll( 'a[href^="http"]:not([href*="example.com"])' ).forEach(anchor => anchor.setAttribute( 'rel', 'noopener noreferrer nofollow' )); 
  */
